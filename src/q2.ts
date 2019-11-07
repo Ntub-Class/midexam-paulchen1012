ded9025ee7bfe7da502e9b5b706f3a274f4edab4
bobo <N1086717@ntub.edu.tw>
2019-11-07 20:30:34 +0800

    期中考

--- src/q2.ts
+++ src/q2.ts
@@ -1,3 +1,7 @@
 // 題目說明: 請使用套件readline-sync跟node-qrcode，輸入文字來產生QRCODE
 // 輸入說明: 請打上任意文字，並產生QRCODE
 // 輸出說明: 請輸出檔名為QRCODE.png圖片
+import * as QRCode from 'qrcode';     
+
+
+QRCode.toFile('./QRCODE.png','拜託讓我PASS')
--- src/q3.ts
+++ src/q3.ts
@@ -1,6 +1,33 @@
 // 題目說明: 請建立兩個class, Animal跟Bird, Bird繼承自Animal
-// Anical class說明: 有型態為string的name屬性，跟eat方法, 呼叫eat方法時請輸出 I'm {name} , I can eat~~
+// Animal class說明: 有型態為string的name屬性，跟eat方法, 呼叫eat方法時請輸出 I'm {name} , I can eat~~
+
+class Animal {
+    name: string;
+    constructor(n: string) {
+        this.name = n;
+    }
+    eat(){
+        console.log("I'm ",this.name,", I can eat");
+    }
+}
 // Bird class說明: 有型態為string的color屬性，跟fly方法， 呼叫fly方法時請輸出 I can fly
 // Bird class說明: 請複寫父類別的eat方法，呼叫Bird的eat方法時必須先印出 I override it，
 //  然後再呼叫父類別的eat 輸出 I'm {name} , I can eat~~
+class Bird extends Animal{
+    constructor(name:string,public color: string){
+        super(name);
+    }
+    fly(){
+        console.log("I can fly");
+    }
+    eat(){
+        super.eat();
+        console.log('I override it， ');
+        super.eat();
+    }
+}
+
 // 最後請建立一個Bird實體，並呼叫eat跟fly方法
+let Bird1 = new Bird('金絲雀','金色');
+Bird1.eat();
+Bird1.fly();
